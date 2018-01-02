8376912c87d8e94f60a08866e03e033445565976
feildmaster <feildmaster@users.noreply.github.com>
2018-01-02 13:34:09 -0600

    feat: util.initialize, for FirebaseAdmin

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const ensureRequired = require('./lib/ensureRequired');
 const PresenceSystem = require('./lib/presenceSystem');
 const getReply = require('./lib/getReply');
+const initialize = require('./lib/initialize');
 
 module.exports = {
   ensureRequired,
   PresenceSystem,
   getReply,
+  initialize,
 };
