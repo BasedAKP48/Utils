35aa1a9d599445c5dd72405ffe1a51ffcd212950
feildmaster <feildmaster@users.noreply.github.com>
2018-01-02 19:02:42 -0600

    feat: utils.getCID

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const ensureRequired = require('./lib/ensureRequired');
 const PresenceSystem = require('./lib/presenceSystem');
 const getReply = require('./lib/getReply');
 const initialize = require('./lib/initialize');
+const getCID = require('./lib/getCID');
 
 module.exports = {
   ensureRequired,
   PresenceSystem,
   getReply,
   initialize,
+  getCID,
 };
