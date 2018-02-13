16d859e7246e45e2a472e69cb3864b4fc9845d6b
feildmaster <akp48@feildmaster.com>
2018-02-13 11:40:21 -0600

    chore: ordering
    Priority:
    - Classes
    - Alphabetical

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
+const Connector = require('./lib/plugin/Connector');
+const Plugin = require('./lib/plugin/Plugin');
+const MessagingSystem = require('./lib/messagingSystem');
+const Module = require('./lib/plugin/Module');
+const PresenceSystem = require('./lib/presenceSystem');
 const ensureRequired = require('./lib/ensureRequired');
-const get = require('./lib/safeFirebaseObject');
 const getCID = require('./lib/getCID');
 const getReply = require('./lib/getReply');
 const initialize = require('./lib/initialize');
-const MessagingSystem = require('./lib/messagingSystem');
-const PresenceSystem = require('./lib/presenceSystem');
-const Plugin = require('./lib/plugin/Plugin');
-const Connector = require('./lib/plugin/Connector');
-const Module = require('./lib/plugin/Module');
 const move = require('./lib/move');
+const get = require('./lib/safeFirebaseObject');
 
 module.exports = {
-  Plugin,
   Connector,
-  Module,
+  Plugin,
   MessagingSystem,
+  Module,
   PresenceSystem,
   ensureRequired,
   getCID,
   getReply,
   initialize,
   move,
+  isUnsafe: get.isUnsafe,
   safeObject: get.safeObject,
   safeString: get.safeString,
-  isUnsafe: get.isUnsafe,
 };
