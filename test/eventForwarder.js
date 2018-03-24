e9231bf65ed696bb445fdc1a77e1e568752afecc
feildmaster <akp48@feildmaster.com>
2018-03-24 00:27:37 -0500

    fix(eventForwarder): forwarded event prefix must be a string

--- test/eventForwarder.js
+++ test/eventForwarder.js
@@ -52,16 +52,23 @@ describe('eventForwarder', () => {
       forward(event1, event2, 'forwarded');
       event2.once('forwardedevent', () => { forwarded = true; });
       event2.once('event', () => { event = true; });
-      expect(event1.emit('event')).to.be.true;
-      expect(forwarded).to.be.true;
-      expect(event).to.be.false;
+      event1.emit('event');
+      expect(forwarded, 'Failed to prefix event').to.be.true;
+      expect(event, 'Forwarded unprefixed event').to.be.false;
     });
-    it('should not be forwarded', () => {
+    it('should be properly forwarded or disabled', () => {
       let disabled = false;
+      let called = false;
+      let badPrefix = false;
       forward(event1, event2, ['disabled']);
       event2.once('disabled', () => { disabled = true; });
-      expect(event1.emit('disabled')).to.be.false;
-      expect(disabled).to.be.false;
+      event2.once('event', () => { called = true; });
+      event2.once('disabledevent', () => { badPrefix = true; });
+      event1.emit('disabled');
+      event1.emit('event');
+      expect(disabled, 'Forwarded disabled event').to.be.false;
+      expect(badPrefix, 'Forwarded with a bad prefix').to.be.false;
+      expect(called, 'Failed to forward event').to.be.true;
     });
     it('should still work when prefix and except are passed', () => {
       let enabled = false;
@@ -69,17 +76,17 @@ describe('eventForwarder', () => {
       forward(event1, event2, { prefix: 'event-', except: ['disabled'] });
       event2.once('event-enabled', () => { enabled = true; });
       event2.once('event-disabled', () => { disabled = true; });
-      expect(event1.emit('enabled')).to.be.true;
-      expect(event1.emit('disabled')).to.be.false;
-      expect(enabled).to.be.true;
-      expect(disabled).to.be.false;
+      event1.emit('enabled');
+      event1.emit('disabled');
+      expect(enabled, 'Failed to forward event').to.be.true;
+      expect(disabled, 'Forwarded disabled event').to.be.false;
     });
     it('should allow except to be a string', () => {
       let disabled = false;
       forward(event1, event2, { prefix: 'event-', except: 'disabled' });
       event2.once('event-disabled', () => { disabled = true; });
-      expect(event1.emit('disabled')).to.be.false;
-      expect(disabled).to.be.false;
+      event1.emit('disabled');
+      expect(disabled, 'Forwarded disabled event').to.be.false;
     });
   });
 });
